627ce95434eead3554367c1a835dc9e9c24107c6
Kevin Sanders <>
2022-01-09 14:46:56 +0100

    feat: add order placing

--- src/pages/OrderInfo.tsx
+++ src/pages/OrderInfo.tsx
@@ -2,56 +2,62 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import '../styles/OrderInfo.css';
 
-interface IOrderInfo {
-
-}
 interface IOrder {
     address: string;
     tel: string;
     email: string;
     name: string;
-    pizzaSettings: Array<IData>
+    pizzaSettings: Array<IPizzaSettings>
     price: number;
 }
-interface IData {
+interface IPizzaSettings {
     id: string;
     quantity: string;
     size: string;
     extrasIds: Array<string>;
-    defaultPrice: string;
     finalPrice: string;
-    extrasPrice: string;
 }
 
-const OrderInfo: React.FunctionComponent<IOrderInfo> = (props) => {
+const OrderInfo: React.FunctionComponent = () => {
 
     const [info, setInfo] = useState<IOrder | null>(null);
     
     const placeOrder = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         const form = e.target as HTMLFormElement;
-        const address = (form.querySelector('.order-info__address') as HTMLInputElement).value;
-        const tel = (form.querySelector('.order-info__tel') as HTMLInputElement).value;
-        const email = (form.querySelector('.order-info__email') as HTMLInputElement).value;
-        const name = (form.querySelector('.order-info__name') as HTMLInputElement).value;
+        try {
+            const address = (form.querySelector('.order-info__address') as HTMLInputElement).value;
+            const tel = (form.querySelector('.order-info__tel') as HTMLInputElement).value;
+            const email = (form.querySelector('.order-info__email') as HTMLInputElement).value;
+            const name = (form.querySelector('.order-info__name') as HTMLInputElement).value;
 
-        const cartString = localStorage.getItem('cart') as string;
-        const pizzaSettings = JSON.parse(cartString) as Array<IData>;
+            const cartString = localStorage.getItem('cart') as string;
+            const pizzaSettings = JSON.parse(cartString) as Array<IPizzaSettings>;
 
-        let price = 0;
+            let price = 0;
 
-        pizzaSettings.forEach((e: IData) => {
-            price += Number(e.finalPrice);
-        });
+            pizzaSettings.forEach((e: IPizzaSettings) => {
+                price += Number(e.finalPrice);
+            });
 
-        setInfo({ address, tel, email, name, pizzaSettings, price });
+            setInfo({ address, tel, email, name, pizzaSettings, price });
+        } catch(e) {
+            const tel = form.querySelector('.order-info__tel') as HTMLInputElement;
+            const errorMessage = tel.nextElementSibling as HTMLSpanElement;
+            errorMessage.style.display = 'block';
+        }
     }
 
     useEffect(() => {
         console.log(info);
-        axios.post('http://localhost:8081/placeOrder', info);
+        sendRequest();
     }, [info]);
     
+    const sendRequest = async () => {
+        const res = await axios.post('http://localhost:8081/placeOrder', info);
+        console.log(res.data);
+    }
+
     return (
         <div className="order-info">
             <form onSubmit={placeOrder} className="order-info__form">
@@ -59,7 +65,8 @@ const OrderInfo: React.FunctionComponent<IOrderInfo> = (props) => {
                     <input type="text" placeholder="Street, house number" className="order-info__address" required />
                 </label>
                 <label className="order-info__label">Phone number:
-                    <input type="tel" placeholder="+48 000 000 000" className="order-info__tel" required />
+                    <input type="tel" placeholder="+48 000 000 000" pattern="+48 [0-9]{3} [0-9]{3} [0-9]{3}" className="order-info__tel" required />
+                    <span className="order-info__tel-error">Incorrect phone number. Pattern: "+48 000 000 000"</span>
                 </label>
                 <label className="order-info__label">Email:
                     <input type="email" placeholder="example@gmail.com" className="order-info__email" required />
